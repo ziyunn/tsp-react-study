270a2471fb0e451069149fe386121caa6e0842ab
kangtaewan <bras1717@naver.com>
2022-05-06 21:16:09 +0900

    어바웃 상단빼고 작업완료

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,21 +1,244 @@
-import React from 'react';
+import React from "react";
+import { Title1, Title3, Body1, Body2, Body3, calcRem } from "../../style/font";
+import { darkGray, yellow, gray } from "../../style/color";
+import parse from "html-react-parser";
+import styled from "styled-components";
+
+const businessList = [
+  "PRODUCTION",
+  "BRAND MARKING",
+  "MODEL AGENCY",
+  "MODEL ACADEMY",
+  "TSP STUDIO",
+];
+
+const productionList = [
+  {
+    ID: "1",
+    ABBREVIATION: "F.",
+    TITLE: "FASHION SHOW PRODUCTION",
+    SUBTITLE:
+      "새로운 기획방법과 연출로 클라이언트와의<br/>지속적인 커뮤니케이션",
+    TXT:
+      "디자이너&브랜드패션쇼<br/>" +
+      "패션디자인&모델 콘테스트<br/>" +
+      "브랜드 런칭 프로모션<br/>" +
+      "대학교졸업작품패션쇼<br/>" +
+      "시니어모델패션쇼브랜드품평회<br/>" +
+      "기업행사 및 컨퍼런스",
+  },
+  {
+    ID: "2",
+    ABBREVIATION: "M.",
+    TITLE: "MODEL MANAGEMENT",
+    SUBTITLE:
+      "모델의 전체적인 매니징과 각종 광고 및<br/>" +
+      "브랜드 촬영을 통한 모델 토탈 마케팅",
+    TXT:
+      "패션모델 매니지먼트<br/>" +
+      "광고모델 에이전시<br/>" +
+      "패션모델 에이전시<br/>" +
+      "연기자 및 모델 엔터테인먼트<br/>" +
+      "시니어모델 매니지먼트 및 에이전시",
+  },
+  {
+    ID: "3",
+    ABBREVIATION: "A.",
+    TITLE: "MODEL ACADEMY",
+    SUBTITLE:
+      "전문적인 패션모델 교육과 교수출신<br/>" + "강사진들의 퀄리티 높은 교육",
+    TXT:
+      "패션모델 아카데미<br/>" +
+      "시니어모델 아카데미<br/>" +
+      "키즈모델 아카데미<br/>" +
+      "연기 아카데미<br/>" +
+      "모델 포트폴리오",
+  },
+  {
+    ID: "4",
+    ABBREVIATION: "B.",
+    TITLE: "BRAND MAKATING",
+    SUBTITLE:
+      "브랜드의 마케팅 기획으로<br/>" + "최고의 만족도로 보답하고 있습니다",
+    TXT:
+      "브랜드 기획<br/>" +
+      "바이럴 마케팅<br/>" +
+      "영상 기획 및 연출<br/>" +
+      "SNS 마케팅",
+  },
+];
+
+const AboutWrap = styled.section`
+  background: #000000;
+  text-align: center;
+
+  .about__title {
+    position: relative;
+
+    &:after {
+      display: block;
+      margin: ${calcRem(16)}rem auto;
+      width: ${calcRem(2)}rem;
+      height: ${calcRem(80)}rem;
+      background: ${darkGray};
+      content: "";
+    }
+  }
+
+  .about__txt {
+    margin-top: ${calcRem(16)}rem;
+  }
+
+  .about__point {
+    margin-bottom: ${calcRem(16)}rem;
+  }
+
+  .about__production + .about__production {
+    margin-top: ${calcRem(16)}rem;
+  }
+`;
+const AboutArea = styled.section`
+  padding: ${calcRem(40)}rem ${calcRem(16)}rem;
+
+  &.type-yellow {
+    background: ${yellow};
+  }
+`;
+
+const AboutList = styled.ul`
+  margin-left: -${calcRem(8)}rem;
+  justify-content: center;
+
+  .about {
+    &__item {
+      display: inline-block;
+      width: 50%;
+      padding-left: ${calcRem(8)}rem;
+      color: #fff;
+      margin-top: ${calcRem(16)}rem;
+      box-sizing: border-box;
+    }
+
+    &__box {
+      position: relative;
+      width: 100%;
+      padding-bottom: 100%;
+    }
+
+    &__circle {
+      display: flex;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      align-items: center;
+      justify-content: center;
+      background: #000;
+    }
+  }
+`;
+
+const aboutList = () => {
+  return (
+    <AboutList className="about__list">
+      {businessList.map((item, idx) => {
+        return (
+          <li className="about__item" key={idx}>
+            <div className="about__box">
+              <Body1 className="about__circle">{item}</Body1>
+            </div>
+          </li>
+        );
+      })}
+    </AboutList>
+  );
+};
+
+const aboutProductions = () => {
+  return (
+    <>
+      {productionList.map((item) => {
+        return (
+          <div className="about__production" key={item.ID}>
+            <Title1
+              as="h4"
+              weight="700"
+              color={yellow}
+              className="font-abril about__point"
+            >
+              {item.ABBREVIATION}
+            </Title1>
+            <Body1 as="p" color="#fff" weight="700">
+              {item.TITLE}
+            </Body1>
+            <Body2 as="p" color="#fff">
+              {parse(item.SUBTITLE)}
+            </Body2>
+            <Body1 as="p" color={gray} className="about__txt">
+              {parse(item.TXT)}
+            </Body1>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 const About = (props) => {
-	return(
-		<>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-			about<br/>
-
-		</>
-	);
+  return (
+    <>
+      <AboutWrap>
+        <AboutArea>
+          <Title3 as="h3" weight="700" color="#fff" className="about__title">
+            ABOUT TSP
+          </Title3>
+          <Title3 as="h4" weight="700" color="#fff">
+            THE SHOW PROJECT
+          </Title3>
+          <Title3 as="p" color="#fff">
+            더쇼프로젝트
+          </Title3>
+          <Body2 color="#fff" className="about__txt">
+            국내외 패션쇼 연출 및 기획, 광고, 잡지, 화보 등 모델 전반의
+            비즈니스는 물론 각종 미디어, 모델 교육과 다양한 컨텐츠를 보유하고
+            있습니다. 하나의 행사를 진행함에 있어 클라이언트와 상호간의 ‘공생’
+            ‘상생’을 기본으로 양질의 멀티 컨텐츠를 통해 새로운 가치를 창출할
+            것을 약속합니다.
+          </Body2>
+          <Body3 color={gray} className="about__txt">
+            The company has various contents in modeling education, directing
+            and planning fashion shows in domestic and overseas. Also various
+            contents of overall model businesses such as the media,
+            advertisements, magazines and pictorials. We create new values
+            through multi-contents based on synergies among clients and events.
+          </Body3>
+        </AboutArea>
+        <AboutArea className="type-yellow">
+          <Title3 as="h3" weight="700" className="about__title">
+            BUSINESS AREA
+          </Title3>
+          <Title3 as="p" weight="700">
+            Various Contents -<br />
+            Communication -<br />
+            Professional
+          </Title3>
+          <Body2 className="about__txt">
+            더쇼프로젝트의 다양한 컨텐츠를 활용한 전문성과 각각의 분야에 초점을
+            맞춰 지속 가능하고 클라이언트가 선택한 파트너가 되는 것입니다.
+          </Body2>
+          {aboutList()}
+        </AboutArea>
+        <AboutArea>
+          <Title3 as="h3" weight="700" color="#fff" className="about__title">
+            PRODUCTION
+          </Title3>
+          {aboutProductions()}
+        </AboutArea>
+      </AboutWrap>
+    </>
+  );
 };
 
 export default About;
--- src/style/font.js
+++ src/style/font.js
@@ -4,6 +4,15 @@ import {calcRem,calclH} from "./function";
 const defaultLetter = 0.06;
 
 
+const Title1 = styled.p`
+  font-size:${calcRem(60)}rem;
+  line-height:${calclH(70,60)}; 
+  letter-spacing: ${defaultLetter}rem;
+  font-weight: ${(props) => props.weight || "400"};
+  color: ${(props) => props.color || "inherit"};
+  text-transform: ${(props)=> props.upper ? "uppercase" : "none"};
+  margin: ${(props) => props.margin || "0 0 0 0"};
+`;
 
 const Title2 = styled.p`
   font-size:${calcRem(40)}rem;
@@ -32,6 +41,16 @@ const Body1 = styled.p`
   margin: ${(props) => props.margin || "0 0 0 0"};
 `;
 
+const Body2 = styled.p`
+  font-size:${calcRem(14)}rem;
+  line-height:${calclH(24,14)};
+  letter-spacing: ${defaultLetter}rem;
+  font-weight: ${(props) => props.weight || "400"};
+  color: ${(props) => props.color || "inherit"};
+  margin: ${(props) => props.margin || "0 0 0 0"};
+`;
+
+
 const Body3 = styled.p`
   font-size:${calcRem(12)}rem;
   line-height:${calclH(22,12)};
@@ -41,4 +60,4 @@ const Body3 = styled.p`
   margin: ${(props) => props.margin || "0 0 0 0"};
 `;
 
-export {Title2, Title3, Body1, Body3, calcRem, calclH}
+export {Title1, Title2, Title3, Body1, Body2, Body3, calcRem, calclH}
